d68a73e87856a553a059bcbb6727174807682682
syousefi <safoora.yousefi1192@gmail.com>
2014-09-19 20:43:17 -0400

    Safoora's first commit:D

--- FFF/src/edu/emory/fff/SettingsActivity.java
+++ FFF/src/edu/emory/fff/SettingsActivity.java
@@ -18,7 +18,7 @@ import android.preference.RingtonePreference;
 import android.text.TextUtils;
 
 import java.util.List;
-
+//My footprint
 /**
  * A {@link PreferenceActivity} that presents a set of application settings. On
  * handset devices, settings are presented as a single list. On tablets,
