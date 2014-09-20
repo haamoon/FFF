54d04c55a1e19e72e496cc18144994b052fec707
Mehrdad Farajtabar <mehrdad.farajtabar@gmail.com>
2014-09-20 16:48:23 -0400

    Improved Parser and Event

--- FFF/src/edu/emory/fff/parser/Event.java
+++ FFF/src/edu/emory/fff/parser/Event.java
@@ -64,22 +64,3 @@ public class Event {
 	}
 	
 }
-
-class Highlight {
-	private int startIndex;
-	private int endIndex;
-	
-	public int getStartIndex() {
-		return startIndex;
-	}
-	public void setStartIndex(int startIndex) {
-		this.startIndex = startIndex;
-	}
-	public int getEndIndex() {
-		return endIndex;
-	}
-	public void setEndIndex(int endIndex) {
-		this.endIndex = endIndex;
-	}
-	
-}
---
+++ FFF/src/edu/emory/fff/parser/Highlight.java
@@ -0,0 +1,20 @@
+package edu.emory.fff.parser;
+
+public class Highlight {
+	private int startIndex;
+	private int endIndex;
+	
+	public int getStartIndex() {
+		return startIndex;
+	}
+	public void setStartIndex(int startIndex) {
+		this.startIndex = startIndex;
+	}
+	public int getEndIndex() {
+		return endIndex;
+	}
+	public void setEndIndex(int endIndex) {
+		this.endIndex = endIndex;
+	}	
+}
+
