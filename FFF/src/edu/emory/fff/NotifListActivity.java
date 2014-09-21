055a29611d9b4c81f331349f39df10515cfd869b
syousefi <safoora.yousefi1192@gmail.com>
2014-09-20 21:27:30 -0400

    ArrayAdapter Overridden

--- FFF/src/edu/emory/fff/NotifListActivity.java
+++ FFF/src/edu/emory/fff/NotifListActivity.java
@@ -3,8 +3,11 @@ package edu.emory.fff;
 
 
 import java.util.ArrayList;
+import java.util.Date;
+
 
 import edu.emory.fff.parser.Event;
+import edu.emory.fff.util.CustomAdapter;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
@@ -20,11 +23,12 @@ public class NotifListActivity extends ListActivity {
 		for (int i = 0; i < 6; i++)
 		{
 			Event e = new Event();
-			e.setBody("Event" + String.valueOf(i));
+			e.setTitle("Event" + String.valueOf(i));
+			e.setDate(new Date(20, 0, i));
 			events.add(e);
 		}
 		
-		ArrayAdapter<Event> adapter = new ArrayAdapter<Event> (this, R.layout.activity_list_item, R.id.eventtxt, events);
+		ArrayAdapter<Event> adapter = new CustomAdapter(this, R.layout.activity_list_item, events);//<Event> (this, R.layout.activity_list_item, events);
 		setListAdapter(adapter);
 	}
 }
---
+++ FFF/src/edu/emory/fff/util/CustomAdapter.java
@@ -0,0 +1,62 @@
+package edu.emory.fff.util;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import edu.emory.fff.R;
+import edu.emory.fff.parser.Event;
+
+public class CustomAdapter extends ArrayAdapter<Event> {
+    private final Context context;
+    private final ArrayList<Event> events;
+    private final int layoutResourceId;
+
+    public CustomAdapter(Context context, int layoutResourceId, ArrayList<Event> events) {
+        super(context, layoutResourceId, events);
+        this.context = context;
+        this.events = events;
+        this.layoutResourceId = layoutResourceId;
+    }
+
+    @Override
+    public View getView (int position, View convertView, ViewGroup parent) 
+    {
+        View row = convertView;
+        ViewHolder holder = null;
+
+        if(row == null)
+        {
+            LayoutInflater inflater = ((Activity)context).getLayoutInflater();
+            row = inflater.inflate(layoutResourceId, parent, false);
+
+            holder = new ViewHolder();
+            holder.textView1 = (TextView)row.findViewById(R.id.eventtxt);
+            holder.textView2 = (TextView)row.findViewById(R.id.date);
+            //holder.textView3 = (TextView)row.findViewById(R.id.text3);
+            row.setTag(holder);
+        }
+        else
+        {
+            holder = (ViewHolder)row.getTag();
+        }
+
+        Event e = events.get(position);
+
+        holder.textView1.setText(e.getTitle());
+        holder.textView2.setText(e.getDate().toString());
+        
+        return row;
+    }
+
+    static class ViewHolder
+    {
+        TextView textView1;
+        TextView textView2;
+    }
+}
